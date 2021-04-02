d0cb0ca425af4cba67e1d91543698292f49cd3b4
asheph02 <>
2021-04-01 21:47:29 -0400

    Bluetooth

--- app/src/main/java/com/uoguelph/feedbackloop/MainActivity.java
+++ app/src/main/java/com/uoguelph/feedbackloop/MainActivity.java
@@ -27,6 +27,24 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import static android.content.ContentValues.TAG;
+
 class quickChart {
     static private AppCompatActivity activity;
 
@@ -68,13 +86,12 @@ class quickChart {
         xAxis.setDrawLabels(false);
         xAxis.setTextColor(Color.WHITE);
 
-
         YAxis leftAxis = chart.getAxisLeft();
         leftAxis.setEnabled( true );
         leftAxis.setDrawGridLines( true );
         leftAxis.setDrawLabels( true );
-        leftAxis.setAxisMaximum( 1.1f );
-        leftAxis.setAxisMinimum( -1.1f );
+        leftAxis.setAxisMaximum( 1.0f );
+        leftAxis.setAxisMinimum( -1.0f );
         leftAxis.setTextColor(Color.WHITE);
         leftAxis.setTextSize(20f);
 
@@ -82,7 +99,6 @@ class quickChart {
         chart.getAxisRight().setEnabled( false );
         chart.getLegend().setEnabled( false );
 
-        double initialX = Math.random() * Math.PI * 2;
         entry.add( new Entry( 0f, 0f ));
 
         dataSet = new LineDataSet( entry, "Values");
@@ -92,7 +108,7 @@ class quickChart {
         dataSet.setDrawValues( false );
         dataSet.setValueTextColor(Color.WHITE);
 
-          lineData = new LineData( dataSet );
+        lineData = new LineData( dataSet );
         chart.setData(lineData);
 
         //BACKGROUND
@@ -106,19 +122,18 @@ class quickChart {
     private void AddEntry(double maxAlert, double minAlert, String parameter) {
 
         int numEntries = entry.size();
-        // float xValue = entry.get( entry.size() - 1 ).getX();
         float xValue = (float)( numEntries * increment );
 
 
         // TODO: CHANGE NEXT LINE
         if(parameter=="Pressure"||parameter=="Flow"){
-            lineData.addEntry( new Entry( (float)numEntries, (float)Math.sin(xValue)), 0 );
+            lineData.addEntry(new Entry((float) numEntries, (float) Math.sin(xValue)), 0);
             lineData.notifyDataChanged();
             chart.notifyDataSetChanged();
             // 500 +-= PI * 2 * 4 * 10
             chart.setVisibleXRange( 500f, 500f );
             chart.moveViewToX(numEntries);
-            if((float)Math.sin(xValue)>maxAlert||(float)Math.sin(xValue)<minAlert) {
+            if ((float) Math.sin(xValue) > maxAlert || (float) Math.sin(xValue) < minAlert) {
                 chart.setBackgroundColor(Color.RED);
             }
             else {
@@ -130,13 +145,9 @@ class quickChart {
             float area = 0;
 
             if (numEntries>40) {
-
-
                 for (int i = 1; i < 40; i++) {
-
                     float width = (float)Math.sin(xValue -(i*increment))+(float)Math.sin(xValue -((i+1)*increment))/2;
                     area = area + (float) increment * width;
-
                 }
             }
             //now we are adding the data
@@ -146,6 +157,8 @@ class quickChart {
             // 500 +-= PI * 2 * 4 * 10
             chart.setVisibleXRange( 500f, 500f );
             chart.moveViewToX(numEntries);
+            chart.getAxisLeft().setAxisMaximum( 3.0f );
+            chart.getAxisLeft().setAxisMinimum( -3.0f );
             if(area>maxAlert||area<minAlert) {
                 chart.setBackgroundColor(Color.RED);
             }
@@ -184,13 +197,23 @@ public class MainActivity extends AppCompatActivity { //implements AdapterView.O
     private quickChart flowChart;
     private Spinner spinner; //new
     private ImageView textPPT; //new
-    private double maxPressureAlert = 0.5;
-    private double minPressureAlert= -0.5;
+    private double maxPressureAlert = 0.8;
+    private double minPressureAlert= -0.8;
     private double maxFlowAlert=1;
     private double minFlowAlert=-1;
-    private double maxVolumeAlert = 1;
-    private double minVolumeAlert = -1;
-    //private Pressure = "Pressure"
+    private double maxVolumeAlert = 3;
+    private double minVolumeAlert = -3;
+
+    private String deviceName = null;
+    private String deviceAddress;
+    public static Handler handler;
+    public static BluetoothSocket mmSocket;
+    public static ConnectedThread connectedThread;
+    public static CreateConnectThread createConnectThread;
+
+    private final static int CONNECTING_STATUS = 1; // used in bluetooth handler to identify message status
+    private final static int MESSAGE_READ = 2; // used in bluetooth handler to identify message update
+
 
 
 
@@ -205,7 +228,60 @@ public class MainActivity extends AppCompatActivity { //implements AdapterView.O
         flowChart = new quickChart( findViewById( R.id.chartFlow), Color.RED );
         //chart volume = integral of chart flow
         volumeChart = new quickChart( findViewById( R.id.chartVolume), Color.YELLOW );
-        //volumeChart = new quickChart //(in progress)
+
+
+        // BT UI Initialization
+        final Button buttonConnect = findViewById(R.id.buttonConnect);
+
+        // If a bluetooth device has been selected from SelectDeviceActivity
+        deviceName = getIntent().getStringExtra("deviceName");
+        if (deviceName != null){
+            // Get the device address to make BT Connection
+            deviceAddress = getIntent().getStringExtra("deviceAddress");
+            buttonConnect.setEnabled(false);
+
+            /*
+            When "deviceName" is found
+            the code will call a new thread to create a bluetooth connection to the
+            selected device (see the thread code below)
+             */
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            createConnectThread = new CreateConnectThread(bluetoothAdapter,deviceAddress);
+            createConnectThread.start();
+        }
+
+        handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg){
+                switch (msg.what){
+                    case CONNECTING_STATUS:
+                        switch(msg.arg1){
+                            case 1:
+                                buttonConnect.setEnabled(true);
+                                break;
+                            case -1:
+                                buttonConnect.setEnabled(true);
+                                break;
+                        }
+                        break;
+
+                    case MESSAGE_READ:
+                        String arduinoMsg = msg.obj.toString(); // Read message from Arduino
+                        break;
+                }
+            }
+        };
+
+        // Select Bluetooth Device
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Move to adapter list
+                Intent intent = new Intent(MainActivity.this, SelectDeviceActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         pressureChart.Start(maxPressureAlert,minPressureAlert,"Pressure");
         flowChart.Start(maxFlowAlert,minFlowAlert,"Flow");
@@ -270,6 +346,153 @@ public class MainActivity extends AppCompatActivity { //implements AdapterView.O
                 }
         );
 
+    }
+
+
+
+    /* ============================ Thread to Create Bluetooth Connection =================================== */
+    public static class CreateConnectThread extends Thread {
+
+        public CreateConnectThread(BluetoothAdapter bluetoothAdapter, String address) {
+            /*
+            Use a temporary object that is later assigned to mmSocket
+            because mmSocket is final.
+             */
+            BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
+            BluetoothSocket tmp = null;
+            UUID uuid = bluetoothDevice.getUuids()[0].getUuid();
+
+            try {
+                /*
+                Get a BluetoothSocket to connect with the given BluetoothDevice.
+                Due to Android device varieties,the method below may not work fo different devices.
+                You should try using other methods i.e. :
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+                 */
+                tmp = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(uuid);
+
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            bluetoothAdapter.cancelDiscovery();
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+                Log.e("Status", "Device connected");
+                handler.obtainMessage(CONNECTING_STATUS, 1, -1).sendToTarget();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                    Log.e("Status", "Cannot connect to device");
+                    handler.obtainMessage(CONNECTING_STATUS, -1, -1).sendToTarget();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            connectedThread = new ConnectedThread(mmSocket);
+            connectedThread.run();
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+    }
+
+    /* =============================== Thread for Data Transfer =========================================== */
+    public static class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes = 0; // bytes returned from read()
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    /*
+                    Read from the InputStream from Arduino until termination character is reached.
+                    Then send the whole String message to GUI Handler.
+                     */
+                    buffer[bytes] = (byte) mmInStream.read();
+                    String readMessage;
+                    if (buffer[bytes] == '\n'){
+                        readMessage = new String(buffer,0,bytes);
+                        Log.e("Arduino Message",readMessage);
+                        handler.obtainMessage(MESSAGE_READ,readMessage).sendToTarget();
+                        bytes = 0;
+                    } else {
+                        bytes++;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(String input) {
+            byte[] bytes = input.getBytes(); //converts entered String into bytes
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                Log.e("Send Error","Unable to send message",e);
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+    /* ============================ Terminate Connection at BackPress ====================== */
+    @Override
+    public void onBackPressed() {
+        // Terminate Bluetooth Connection and close app
+        if (createConnectThread != null){
+            createConnectThread.cancel();
+        }
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
     }
 
 
